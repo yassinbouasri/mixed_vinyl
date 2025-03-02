123b6f9511766682c25e749dac99443afb091bbe
yassinbouasri <79885361+yassinbouasri@users.noreply.github.com>
2025-03-02 01:34:51 +0100

    playing audio with stimulus

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,4 @@
+import './bootstrap.js';
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -7,3 +8,12 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+
+import { startStimulusApp } from '@symfony/stimulus-bridge';
+
+// Lazy-load Stimulus controllers
+export const app = startStimulusApp(require.context(
+    '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
+    true,
+    /\.(j|t)sx?$/
+));
